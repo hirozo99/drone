d48deb7baa27ebc09b12c4cb5a966bc9b6d301d4
hirozo99 <hiro.1999@me.com>
2022-11-25 20:53:52 +0900

    add

---
+++ parrot_anafi/main.py
@@ -0,0 +1,15 @@
+"""離陸した後、マーカーIDを取得するプログラム"""
+import olympe
+import os
+import time
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+
+DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
+
+def test_takeoff():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    assert drone(TakeOff()).wait().success()
+    time.sleep(10)
+    assert drone(Landing()).wait().success()
+    drone.disconnect()
