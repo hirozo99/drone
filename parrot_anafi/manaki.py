d461b42efac9eadcd1eb4593732eeca8d91dbd0a
hirozo99 <hiro.1999@me.com>
2022-12-12 16:14:19 +0900

    add

---
+++ parrot_anafi/manaki.py
@@ -0,0 +1,50 @@
+#マーカーIDと、動画を重ねて表示する
+import cv2
+from cv2 import aruco
+import numpy as np
+import os
+
+# parrot
+import olympe
+import os
+import time
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+
+DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
+
+RTSP_URL ='rtsp://192.168.42.1/live'
+os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS']='rtsp_transport;udp'
+
+
+### --- aruco設定 --- ###
+dict_aruco = aruco.Dictionary_get(aruco.DICT_4X4_50)
+parameters = aruco.DetectorParameters_create()
+
+cap = cv2.VideoCapture(RTSP_URL)
+
+def test_takeoff():
+    assert drone(TakeOff()).wait().success()
+
+def test_landing():
+    assert drone(Landing()).wait().success()
+
+def aruco_landing():
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
+
+        list_ids = np.ravel(ids)
+        print(list_ids)
+        if list_ids[0] == 0:
+            print("着陸体制に入ります！！")
+            test_landing()
+            break
+
+if __name__ == "__main__":
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    test_takeoff()
+    aruco_landing()
+    drone.disconnect()
