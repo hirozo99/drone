6828b4eb2b7e9b5a7ac72aafad778917e875da7e
hirozo99 <hiro.1999@me.com>
2022-12-12 14:06:33 +0900

    add

--- parrot_anafi/test2.py
+++ parrot_anafi/test2.py
@@ -1,12 +1,15 @@
 #マーカーIDと、動画を重ねて表示する
 import cv2
 from cv2 import aruco
-import os
 import numpy as np
-import time
+
 # parrot
-#import olympe
-#from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+import olympe
+import os
+import time
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+
+DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
 
 RTSP_URL ='rtsp://192.168.42.1/live'
 os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS']='rtsp_transport;udp'
@@ -18,25 +21,45 @@ parameters = aruco.DetectorParameters_create()
 
 cap = cv2.VideoCapture(RTSP_URL)
 
+def test_takeoff():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    assert drone(TakeOff()).wait().success()
+    time.sleep(10)
 
-try:
-    while True:
-        ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+def test_landing():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    time.sleep(10)
+    assert drone(Landing()).wait().success()
+    drone.disconnect()
 
-        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
+def aruco_landing():
+    try:
+        while True:
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-        frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
-        cv2.imshow('frame', frame_markers)
-        list_ids = np.ravel(ids)
-        if list_ids[0] == 0:
-            print("着陸体制に入ります！！")
-            pass
+            corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyWindow('frame')
-    cap.release()
-except KeyboardInterrupt:
-    cv2.destroyWindow('frame')
-    cap.release()
+            frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+            cv2.imshow('frame', frame_markers)
+            list_ids = np.ravel(ids)
+            if list_ids[0] == 0:
+                print("着陸体制に入ります！！")
+                test_landing()
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                test_landing()
+                break
+        cv2.destroyWindow('frame')
+        cap.release()
+    except KeyboardInterrupt:
+        cv2.destroyWindow('frame')
+        cap.release()
+        test_landing()
+
+if __name__ == "__main__":
+    test_takeoff()
+    test_landing()
+    aruco_landing()
