54887a2b4a99f7c17e23400b9e6eec635dc278db
katohiro <hiro.1999@me.com>
2023-01-19 14:46:22 +0900

    add

--- parrot_anafi/hirozo.py
+++ parrot_anafi/hirozo.py
@@ -8,15 +8,31 @@ from olympe.messages.ardrone3.Piloting import moveBy
 DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
 H = 5
 
-def main():
+def takeoff():
     drone = olympe.Drone(DRONE_IP)
     drone.connect()
     assert drone(TakeOff()).wait().success()
     time.sleep(5)
+
+def gain_altitude():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
     drone(moveBy(0, 0, -H, 0)).wait().success()
+    time.sleep(5)
+
+def landing():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
     assert drone(Landing()).wait().success()
     drone.disconnect()
 
+def main():
+    takeoff()
+    try:
+        gain_altitude()
+        landing()
+    except KeyboardInterrupt:
+        landing()
 
 if __name__ == "__main__":
     main()
