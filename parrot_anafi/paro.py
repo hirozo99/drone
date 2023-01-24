6ff1dd34e737a19c939a171ba32c0d7115b023ec
katohiro <hiro.1999@me.com>
2023-01-24 12:24:30 +0900

    add

--- parrot_anafi/paro.py
+++ parrot_anafi/paro.py
@@ -11,7 +11,7 @@ from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveBy
 
 # 変数の指定
 DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
-H = 6
+H = 2
 F = 1
 
 RTSP_URL ='rtsp://192.168.42.1/live'
