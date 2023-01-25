e9f2eaf313a4cd01790a5e3a0822a0aa7a2b9b12
katohiro <hiro.1999@me.com>
2023-01-25 21:52:55 +0900

    add

--- parrot_anafi/kubo.py
+++ parrot_anafi/kubo.py
@@ -52,24 +52,25 @@ def main():
     time.sleep(1)
     test_move(drone, 0, 0)
     time.sleep(1)
+
     try:
         while True:
             ret, frame = cap.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-            corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
-            frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
-            cv2.imshow('frame', frame_markers)
-            # video.write(frame)
-            print(corners)
-            list_ids = list(np.ravel(ids))
-            list_ids.sort()
-            print(list_ids)
-
-            if list_ids[0] == 0:
-                print("着陸体制に入ります！！")
-                if list_ids[-1] == 4 and len(list_ids) == 5:
-                    print("--------------------------着陸--------------------------")
-                    break
+            # gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            # corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, dict_aruco, parameters=parameters)
+            # frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+            cv2.imshow('frame', frame) #frame_markers
+            # # video.write(frame)
+            # print(corners)
+            # list_ids = list(np.ravel(ids))
+            # list_ids.sort()
+            # print(list_ids)
+            #
+            # if list_ids[0] == 0:
+            #     print("着陸体制に入ります！！")
+            #     if list_ids[-1] == 4 and len(list_ids) == 5:
+            #         print("--------------------------着陸--------------------------")
+            #         break
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         cv2.destroyWindow('frame')
