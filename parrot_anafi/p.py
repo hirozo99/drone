9142c89bc102fc5d8cd3fa75af9b1bdcf8351a9a
katohiro <hiro.1999@me.com>
2023-01-26 13:37:53 +0900

    add

---
+++ parrot_anafi/p.py
@@ -0,0 +1,42 @@
+# parrot
+import olympe
+import os
+import time
+from olympe.messages.ardrone3.Piloting import TakeOff, Landing, moveBy
+from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
+from olympe.messages.move import extended_move_by
+
+# 変数の指定
+DRONE_IP = os.environ.get("DRONE_IP", "192.168.42.1")
+
+def test_takeoff(drone):
+    print("--------------------test_takeoff--------------------")
+    assert drone(TakeOff()).wait().success()
+    time.sleep(5)
+
+def test_move(drone, F, H):
+    print("--------------------test_move--------------------")
+    drone(
+        extended_move_by(F, 0, -H, 0, 0.7, 0.7, 0.7)
+        >> FlyingStateChanged(state="hovering", _timeout=5)
+    ).wait().success()
+    time.sleep(5)
+
+def test_landing(drone):
+    print("--------------------test_landing--------------------")
+    drone(Landing()).wait().success()
+    drone.disconnect()
+
+def main():
+    drone = olympe.Drone(DRONE_IP)
+    drone.connect()
+    test_takeoff(drone)
+    time.sleep(1)
+    test_move(drone, 0, 1)
+    time.sleep(1)
+    test_move(drone, 1, 0)
+    time.sleep(1)
+    test_landing(drone)
+
+if __name__ == "__main__":
+    main()
